5a54ec2a80c27513897a63187a382ae083b07e52
Renato Domingues Silva <kungfurenato30@gmail.com>
2020-08-13 09:22:59 -0300

    components/header/index.js  pages/styles.module.css

--- components/header/index.js
+++ components/header/index.js
@@ -6,34 +6,34 @@ import Link from 'next/link'
 const Header = () => {
     return (
         <React.Fragment>
-            <div className={styles.wrapper}>
-               <div className={styles.headerSize}>
-                 <div className={styles.headerSize2}>
-                 <img className={styles.headerSize3} src='../../photoBackground.jpg' width='80%' height='100%'/>
+              <div className={styles.wrapper}>
+                  <div className={styles.headerSize}>
+                    <div className={styles.headerSize2}>
+                      <img className={styles.headerSize3} src='../../photoBackground.jpg' width='80%' height='100%'/>
+                    </div>
+                    <div className={styles.headerSize2}>
+                      <img className={styles.headerSize3} src='../../fotoCurri.jpg' alt='foto' width='50%' height='50%'/>
+                    </div>
+                    <div className={styles.headerSize2}>
+                      <img className={styles.headerSize3} src='../../photoBackgroundDev.jpg' width='80%' height='100%'/>
+                    </div>
                   </div>
-                  <div className={styles.headerSize2}>
-                    <img className={styles.headerSize3} src='../../fotoCurri.jpg' alt='foto' width='50%' height='50%'/>
-                  </div>
-                  <div className={styles.headerSize2}>
-                  <img className={styles.headerSize3} src='../../photoBackgroundDev.jpg' width='80%' height='100%'/>
-                  </div>
-               </div>
-            </div>
-            <div className={styles.wrapper}>
-              <div className={styles.div}>
-                <p className={styles.headerP}>Sou casado, tenho 38 anos, 02 filhas, já trabalhei na área como vendedor,
+              </div>
+              <div className={styles.wrapper}>
+                <div className={styles.div}>
+                  <p className={styles.headerP}>Sou casado, tenho 38 anos, 02 filhas, já trabalhei na área como vendedor,
                 subgerente, motorista de aplicativo, operador de empilhadeira e vigilante.
                 Atualmente busco numa empresa uma oportunidade que eu possa
                 desenvolver profissionalmente, demonstrar minhas competências e habilidades,
                 colaborando assim para o crescimento da mesma.</p>
+                </div>
               </div>
-            </div>
-            <div className={styles.wrapper}>
-              <div className={styles.div}>
-                <h1 className={styles.headerH1}>Contate-me</h1>
-                   <p className={styles.headerP1}><a href='https://www.facebook.com/profile.php?id=100013733978537' target='_blank'>Facebook</a> - <a href='https://github.com/RenatoDomingues' target='_blank'>GitHub</a> - <a href='linkedin.com/in/renato-domingues-silva-094b971b0/' target='_blank'>LinkedIn</a> - Ou pelo email kungfurenato30@gmail.com</p>
+              <div className={styles.wrapper}>
+                <div className={styles.div}>
+                  <h1 className={styles.headerH1}>Contate-me</h1>
+                     <p className={styles.headerP1}><a href='https://www.facebook.com/profile.php?id=100013733978537' target='_blank'>Facebook</a> - <a href='https://github.com/RenatoDomingues' target='_blank'>GitHub</a> - <a href='linkedin.com/in/renato-domingues-silva-094b971b0/' target='_blank'>LinkedIn</a> - Ou pelo email kungfurenato30@gmail.com</p>
+                </div>
               </div>
-            </div>
         </React.Fragment>
     )
 }
